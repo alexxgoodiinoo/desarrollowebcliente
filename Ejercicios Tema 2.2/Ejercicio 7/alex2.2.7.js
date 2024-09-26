282898f9a4204c6224c631335a402c34c9a6efb0
Alex Godino <alex.godino.0403@fernando3martos.com>
2024-09-26 08:55:29 +0200

    Ejercicios 2.2 realizados

---
+++ Ejercicios Tema 2.2/Ejercicio 7/alex2.2.7.js
@@ -0,0 +1,19 @@
+//Apartado 1
+let cadena1 = "Hola";
+let cadena2 = "Hola";
+console.log(cadena1==cadena2);
+
+//Apartado 2
+let cadena3 = "Hola";
+let cadena4 = "Hellou";
+console.log(cadena3==cadena4);
+
+//Apartado 3
+let cadena5 = 5;
+let cadena6 = "5";
+console.log(cadena5===cadena6);
+
+//Apartado 4
+let cadena7 = 5;
+let cadena8 = "5";
+console.log(cadena7==cadena8);
